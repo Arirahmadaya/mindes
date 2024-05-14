6659ae1fcd450d414b1016dd6a21760168695be2
istrigojo <massayu197@gmail.com>
2024-05-14 22:45:02 +0700

    progress

---
+++ src/components/Auth.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import { TextareaVariants } from './TextareaVariants';
+
+// const Auth = () => {
+//     return (
+//         <div>
+//             <h1 >Selamat Datang</h1>
+//             <img src="" alt="" />
+//             <div className='text-heading-2'>Kelurahan Kalinyamat Kulon</div>
+//             <div className=''>Masuk</div>
+//             <Textarea variant="standard" label="Usernam" />
+//         </div>
+//     )
+// }
+
+// export default Auth
