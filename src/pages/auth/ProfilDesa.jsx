b98812124ab7fcee854a3ed0488dd5aff96b63c2
Arirahmadaya <82730865+Arirahmadaya@users.noreply.github.com>
2024-05-18 22:23:21 +0700

    add next ui

--- src/pages/auth/ProfilDesa.jsx
+++ src/pages/auth/ProfilDesa.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Cards from '../../components/CardNext'
+import NavbarNext from '../../components/NavbarNext'
 
 const ProfilDesa = () => {
   return (
-    <div>ProfilDesa</div>
+    <>
+    <div className='mb-96'> <NavbarNext /> <Cards />   </div>
+    <div className='mb-96'>asasa</div>
+    </>
   )
 }
 
