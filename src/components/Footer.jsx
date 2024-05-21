0997890bf8a60e83bc452a8efd1af9b458595d73
Arirahmadaya <82730865+Arirahmadaya@users.noreply.github.com>
2024-05-21 18:40:53 +0700

    Resposive foote

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Icon from "react-feather";
 import { Link } from "react-router-dom";
-
 
 const Footer = () => {
   return (
     <>
-      <footer className="flex justify-between bg-primary-40 text-black rounded-t-3xl py-5 px-16">
-        <div>
+      <footer className="flex flex-wrap justify-between bg-primary-40 text-white rounded-t-3xl py-5 px-8 md:px-70">
+        <div className="w-full xl:w-auto flex-shrink-0 mb-4 xl:mb-0">
           <Logo />
-          <br />
-          <Medsos />
+          <div className="hidden xl:block">
+            <Medsos />
+          </div>
         </div>
-        <KontakKelurahan />
-        <KontakPenting />
-        <Jelajahi />
+        <div className="w-full md:hidden text-body-2 ">
+          <Dropdown title="Kontak Kelurahan">
+            <KontakKelurahan />
+          </Dropdown>
+          <Dropdown title="Kontak Penting">
+            <KontakPenting />
+          </Dropdown>
+          <Dropdown title="Jelajahi">
+            <Jelajahi />
+          </Dropdown>
+          <div className="md:block">
+            <Medsos />
+          </div>
+        </div>
+        <div className="hidden md:flex w-full md:w-auto flex-shrink-0 mb-4 md:mb-0">
+          <KontakKelurahan />
+        </div>
+        <div className="hidden md:flex w-full md:w-auto flex-shrink-0 mb-4 md:mb-0">
+          <KontakPenting />
+        </div>
+        <div className="hidden md:flex w-full md:w-auto flex-shrink-0 mb-4 md:mb-0">
+          <Jelajahi />
+        </div>
       </footer>
       <div className="bg-primary-10 text-center py-2">
         🚀 2024 Madilog. All rights reserved
@@ -27,7 +47,7 @@ export default Footer;
 
 export const Logo = () => {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center xl:mb-5">
       <img src="logo/logo_light.png" alt="Logo" className="w-24 h-24 pr-3" />
       <div className="-space-y-2">
         <p className="text-heading-3 font-bold">Kalinyamat Kulon</p>
@@ -45,15 +65,21 @@ export const Logo = () => {
 
 export const Medsos = () => {
   return (
-    <div className="">
+    <div className="text-center xl:text-left mt-4 xl:mt-0">
       <p className="text-xl font-bold mb-2">Media Sosial</p>
-      <div className="flex space-x-4">
-        <Link to="facebook.com">
-          <Icon.Facebook className="text-white w-10 h-10" />
+      <div className="flex justify-center xl:justify-start space-x-4">
+      <Link to= "https://facebook.com" target="_blank">
+          <Icon.Facebook className="text-white w-8 h-8 " />
         </Link>
-        <Icon.Twitter className="text-white w-10 h-10" />
-        <Icon.Instagram className="text-white w-10 h-10" />
-        <Icon.Youtube className="text-white w-10 h-10" />
+        <Link to= "https://twitter.com" target="_blank">
+          <Icon.Twitter className="text-white w-8 h-8" />
+        </Link>
+        <Link to= "https://instagram.com" target="_blank">
+          <Icon.Instagram className="text-white w-8 h-8 " />
+        </Link>
+        <Link to= "https://youtube.com" target="_blank">
+          <Icon.Youtube className="text-white w-8 h-8 " />
+        </Link>
       </div>
     </div>
   );
@@ -61,19 +87,21 @@ export const Medsos = () => {
 
 export const KontakKelurahan = () => {
   return (
-    <div className="">
-      <p className="text-body-1 font-bold">Kontak Kelurahan</p>
-
+    <div >
+  
+      <p className="text-body-1 font-bold mb-4 hidden md:block">
+        Kontak Kelurahan
+      </p>
       <div className="text-body-2">
-        <div className="flex items-center space-x-2 py-1">
+        <div className="flex items-center space-x-2 py-2">
           <Icon.Phone className="text-white w-6 h-6" />
           <p>+62283 311354</p>
         </div>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-2 py-2">
           <Icon.Mail className="text-white w-6 h-6" />
           <p>kelkalinyamatkulon@tegalkota.go.id</p>
         </div>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-2 py-2">
           <Icon.Calendar className="text-white w-6 h-6" />
           <p>
             Senin - Jumat
@@ -81,18 +109,17 @@ export const KontakKelurahan = () => {
             07:00 - 15:00
           </p>
         </div>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-2 py-2">
           <Icon.Briefcase className="text-white w-6 h-6" />
           <p>
             Jalan Ki Hajar Dewantoro <br />
-            No. 109, Tegal 52144{" "}
+            No. 109, Tegal 52144
           </p>
         </div>
       </div>
     </div>
   );
 };
-
 const ListKontak = [
   {
     icon: "src/assets/icon/flame.svg",
@@ -128,14 +155,16 @@ const ListKontak = [
 
 export const KontakPenting = () => {
   return (
-    <div className="p-4">
-      <p className="text-body-1 font-bold mb-4">Kontak Penting</p>
+    <div>
+      <p className="text-body-1 font-bold mb-4 hidden md:block ">
+        Kontak Penting
+      </p>
       <div className="space-y-4">
         {ListKontak.map((kontak, index) => (
           <div key={index} className="flex items-center space-x-2">
             <img src={kontak.icon} alt={kontak.title} className="w-6 h-6" />
             <div>
-              <p className="font-semibold">{kontak.title}</p>
+              <p className="text-body-2 font-semibold">{kontak.title}</p>
               <p>{kontak.number}</p>
             </div>
           </div>
@@ -144,10 +173,11 @@ export const KontakPenting = () => {
     </div>
   );
 };
+
 export const Jelajahi = () => {
   return (
     <div>
-      <p className="text-body-1 font-bold">Jelajahi</p>
+      <p className="text-body-1 font-bold mb-4 hidden md:block">Jelajahi</p>
       <div className="space-y-2 text-body-2">
         <p>Beranda</p>
         <p>Profil Desa</p>
@@ -155,6 +185,29 @@ export const Jelajahi = () => {
         <p>Berita</p>
         <p>Link *</p>
       </div>
+    </div>
+  );
+};
+
+const Dropdown = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="mb-4">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex items-center justify-between w-full p-4 bg-primary-30 rounded-lg text-left focus:outline-none"
+      >
+        <span className="font-bold">{title}</span>
+        <Icon.ChevronDown
+          className={`w-5 h-5 transition-transform ${
+            isOpen ? "transform rotate-180" : ""
+          }`}
+        />
+      </button>
+      {isOpen && (
+        <div className="p-4 bg-primary-30 rounded-md mb-4 mt-2">{children}</div>
+      )}
     </div>
   );
 };
