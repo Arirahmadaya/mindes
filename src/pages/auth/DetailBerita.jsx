ca547d40299d14043e3fe107223b0d4c8b99774b
rajawaliadi <lifecodstore@gmail.com>
2024-05-31 21:09:48 +0700

    detailberita

--- src/pages/auth/DetailBerita.jsx
+++ src/pages/auth/DetailBerita.jsx
@@ -10,6 +10,8 @@ const DeetailBerita = () => {
   return (
     <div>
       <NavbarUser />
+      <div className=" lg:my-[30px] my-[10px]">
+        <div className="md:flex justify-center items-center ">
       <div className="max-w-6xl mx-auto p-4">
         <article className="bg-white p-6 shadow-md">
         <div className="text-black text-[38px] font-bold font-['Open Sans'] leading-[45.60px]">Monitoring posyandu melati 2<br/>desa kalinyamat kulon </div>
