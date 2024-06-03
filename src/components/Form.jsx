c6ac9dd2b7ce8c65edb0dcb1274ace1a72bc4bfd
Arirahmadaya <82730865+Arirahmadaya@users.noreply.github.com>
2024-06-03 19:23:29 +0700

    form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-
+import { Input } from "@nextui-org/react";
 const Form = () => {
+  const variants = ["flat", "bordered", "underlined", "faded"];
   return (
     <div className="w-full h-auto my-16 bg-slate-50 rounded-lg p-5">
       <p className="text-center text-body-1 font-bold">Form</p>
       <div>
-        inpout
+        <div className="w-full flex flex-col gap-4">
+          {variants.map((variant) => (
+            <div
+              key={variant}
+              className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4"
+            >
+              <Input type="email" variant={variant} label="Email" />
+              <Input
+                type="email"
+                variant={variant}
+                label="Email"
+                placeholder="Enter your email"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
